42d4cc0c735070e5776816191bdae3062069097d
qudtjs0753 <qudtjs0753@naver.com>
2021-11-15 23:22:58 +0900

    Baekjoon : 1197번 prim algorithm(No Priority Queue)을 이용한 풀이

--- src/Baekjoon/Algo1197.java
+++ src/Baekjoon/Algo1197.java
@@ -8,7 +8,7 @@ public class Algo1197 {
     static int V, E;
     static final int MAX_V = 100;
     static final int INF = Integer.MAX_VALUE;
-
+    static List<Node>[] adj;
 
     public static class Node implements Comparable<Node> {
         int vertexTo, weight;
@@ -24,37 +24,41 @@ public class Algo1197 {
         }
     }
 
-    static List<Node>[] adj;
 
-    public static long prim(){
+    public static long prim(List<Node> selected){
         boolean[] visited = new boolean[V+1];
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(1, 0));
+        int[] minWeight = new int[V+1];
+        for(int i=1; i<V+1; i++){
+            minWeight[i] = Integer.MAX_VALUE;
+        }
+        int[] parent = new int[V+1];
+        //1번부터 시작
+        minWeight[1] = parent[1] = 0;
+
 
         long ret = 0;
         int count = 0;
 
-        while(!pq.isEmpty()){
-            //priority queue의 첫번째 값 반환 + queue에서 제거
-            //priority queue의 첫번째 값이 가장 weight가 적은 값이므로.
-            //compareTo에 정의됨.
-            Node edge = pq.poll();
-            if(visited[edge.vertexTo])continue;
-
-            ret += edge.weight;
-            visited[edge.vertexTo] = true;
-
-            if(++count == V) return ret;
-            //1번 vertex와 이어진 점들과의 가중치 탐색.
-            for(int i=0; i< adj[edge.vertexTo].size(); i++){
-                Node next = adj[edge.vertexTo].get(i);
-                //만약 이미 트리내부에 있는 점이면 추가하지 않고 넘어간다.
-                if(visited[next.vertexTo]) continue;
-                pq.add(next);
+        for(int iter=0; iter<V; iter++){
+            //모든 점 다 체크
+            //edge 다 돌면서 최소인거 씀.
+            int u = -1;
+            for(int v = 1; v< V+1; v++)
+                if(!visited[v] && (u == -1 || (minWeight[u] > minWeight[v]))) u = v;
+            if(parent[u] != u) selected.add(new Node(parent[u], u));
+            ret += minWeight[u];
+            visited[u] = true;
+            //u에 인접한 간선 (u,v)들을 검사한다.
+            for(int i=0; i< adj[u].size(); i++){
+                int v = adj[u].get(i).vertexTo;
+                int weight = adj[u].get(i).weight;
+                if(!visited[v] && minWeight[v] > weight){
+                    parent[v] = u;
+                    minWeight[v] = weight;
+                }
             }
         }
-
-        return 0;
+        return ret;
     }
 
     public static void main(String[] args) throws IOException {
@@ -63,9 +67,11 @@ public class Algo1197 {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
+        List<Node> selected = new ArrayList<>();
         adj = new ArrayList[V + 1];
-        for (int i=1; i<=V; i++){
-            adj[i] = new ArrayList<>();
+
+        for (int i=0; i<=V; i++){
+            adj[i] = new ArrayList<Node>();
         }
 
         for(int i=0; i<E; i++){
@@ -77,6 +83,8 @@ public class Algo1197 {
             adj[a].add(new Node(b, c));
             adj[b].add(new Node(a, c));
         }
-        System.out.println(prim());
+        int a = 0;
+
+        System.out.println(prim(selected));
     }
 }
