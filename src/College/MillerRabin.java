22805667f4693a46b81cb910246e478c9314d89d
qudtjs0753 <qudtjs0753@naver.com>
2021-11-23 23:28:07 +0900

    College: MillerRabin 소수판별 알고리즘을 이용한 풀이

---
+++ src/College/MillerRabin.java
@@ -0,0 +1,80 @@
+package College;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @Author: kbs
+ */
+public class MillerRabin {
+    static int N;
+    static int[] testcase = {2, 7, 61};
+
+    //N-1 = d x 2^s
+    public static int isPrime(int a){
+        if(N==2 || N==3)return 1;
+        if(N%2==0)return 0;
+        int expo = N-1;
+        int s = divideExponent(expo);
+        int d = expo;
+        //a^d승 만드는 과정.
+        for(int i=0; i<s; i++)
+            d /= 2;
+        long base= powMod(d,a);
+        //case1 : a^d =1(mod n)
+        if(case1(base))return 1;
+
+        //case2 : a^(d x 2^r) = -1(mod n)
+        return case2(base, s, a, d);
+    }
+    public static boolean case1(long base){
+        if(base!=1)
+            return false;
+
+        return true;
+    }
+    public static int case2(long base, int s, int a, int exp){
+        //0<r<s
+        base = base%N;
+        for(int i=0; i<s; i++){
+            if(base==N-1)
+                return 1;
+            base = (base*base)%N;
+        }
+        return 0;
+    }
+
+    public static long powMod(int expo, long num){
+        num = num%N;
+        long base = num;
+        for(int i=1; i<expo; i++){
+            num = (num*base)%N;
+        }
+
+        return num;
+    }
+
+    public static int divideExponent(int expo){
+        int count = 0;
+        while(expo%2==0){
+            count++;
+            expo/=2;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        int ret=0;
+        for(int i=0; i<3; i++){
+            if(N<=testcase[i])break;
+            if((ret=isPrime(testcase[i]))==1){
+                ret = 1;
+                break;
+            }
+        }
+        System.out.println(ret);
+    }
+}
