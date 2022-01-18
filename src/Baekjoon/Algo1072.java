4e490352f1f3109a18a53da91ed8b3e7409c3519
qudtjs0753 <qudtjs0753@naver.com>
2022-01-18 23:01:16 +0900

    Baekjoon : 1072번 이진탐색 풀이

---
+++ src/Baekjoon/Algo1072.java
@@ -0,0 +1,43 @@
+package Baekjoon;
+
+import PL.B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @Author: kbs
+ */
+public class Algo1072 {
+    static int x, y;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+
+        if(x==y) System.out.println(-1);
+        else {
+            int result = -1;
+            int left = 0;
+            int right = (int) 1e9;
+            long z = getPercent(x, y);
+            while(left <= right){
+                int mid = (left + right)/2;
+                if (getPercent(x + mid, y + mid )!=z){
+                    right = mid-1;
+                    result = mid;
+                }else {
+                    left = mid+1;
+                }
+            }
+            System.out.println(result);
+        }
+    }
+
+    static long getPercent(int x, int y){
+        return ((long) y*100/x);
+    }
+}
