d5e92c8f2f1172539e471e8d4a897ace2d837d62
qudtjs0753 <qudtjs0753@naver.com>
2022-02-05 11:40:12 +0900

    Baekjoon : 10816 카운팅정렬.

---
+++ src/Baekjoon/Algo10816.java
@@ -0,0 +1,32 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+/**
+ * @Author: kbs
+ */
+public class Algo10816 {
+    static int N,M;
+    static ArrayList<Integer> arr = new ArrayList<>();
+    static int[] result = new int[20000002];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            result[Integer.parseInt(st.nextToken()) + 10000000]++;
+        }
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<M; i++){
+            sb.append(result[Integer.parseInt(st.nextToken())+10000000]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
