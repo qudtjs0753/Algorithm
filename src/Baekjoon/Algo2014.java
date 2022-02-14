97b933247e9b79ea72c3422b74c04d7b021a8afe
qudtjs0753 <qudtjs0753@naver.com>
2022-02-15 00:01:10 +0900

    Baekjoon : 2014 Priority Queue를 이용한 풀이 개선

--- src/Baekjoon/Algo2014.java
+++ src/Baekjoon/Algo2014.java
@@ -13,9 +13,8 @@ import java.util.StringTokenizer;
  */
 public class Algo2014 {
     static int K, N;
-    static ArrayList<Integer> primes = new ArrayList<>();
-    static PriorityQueue<Integer> result = new PriorityQueue<>(Collections.reverseOrder());
-    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    static ArrayList<Long> primes = new ArrayList<>();
+    static PriorityQueue<Long> pq = new PriorityQueue<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,48 +24,30 @@ public class Algo2014 {
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < K; i++) {
-            int input = Integer.parseInt(st.nextToken());
+            long input = Integer.parseInt(st.nextToken());
             primes.add(input);
-            result.add(input);
             pq.add(input);
         }
 
         Collections.sort(primes);
 
-        findNthNumber();
-
-        System.out.println(result.poll());
+        System.out.println(findNthNumber());
     }
 
-    private static void findNthNumber(){
-        while(!pq.isEmpty()){
-            int current = pq.poll();
-            int idx = 0;
-            for(int i = K-1;i>=0; i--){
-                if(current % primes.get(i) ==0){
-                    idx = i;
-                    break;
-                }
-            }
-            for(int i = idx; i<K; i++){
-                long candidate = (long)current*primes.get(i);
+    private static long findNthNumber(){
+        long ret = 0;
+        while(N-->0){
+            ret = pq.poll();
 
-                //Integer 범위를 벗어나는경우 그냥 out.
-                if(candidate >= Integer.MAX_VALUE);
-                else if(result.size()<N){
-                    pq.add((int)candidate);
-                    result.add((int)candidate);
-                }else{
-                    //저장된 result가 N개를 넘으면 일단 버린다.
-                    if(candidate < result.peek()){
-                        //N개있는데 제일 큰값보다 작은게 들어오면 넣는다.
-                        pq.add((int)candidate);
-                        result.poll();
-                        result.add((int)candidate);
-                    }
-                    if(result.size()>N)result.poll();
-                }
+            for(long next: primes){
+
+                if(ret * next >= ((long)2<<30))break;
+
+                pq.add(next*ret);
+
+                if(ret % next == 0)break;
             }
         }
+        return ret;
     }
 }
