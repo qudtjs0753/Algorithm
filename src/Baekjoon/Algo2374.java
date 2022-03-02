a040797fb7fbd77c14832a9ed00e454419006c48
qudtjs0753 <qudtjs0753@naver.com>
2022-03-03 02:42:26 +0900

    Baekjoon : 2374 최종수정

--- src/Baekjoon/Algo2374.java
+++ src/Baekjoon/Algo2374.java
@@ -3,37 +3,35 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 /**
  * @Author: kbs
  */
 public class Algo2374 {
     static int N;
-    static Stack<Integer> st = new Stack<>();
+    static int minimum;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        st.push(Integer.parseInt(br.readLine()));
+        minimum = Integer.parseInt(br.readLine());
         long result = 0;
-        int max = st.peek();
+        int max = minimum;
 
         for(int i=0; i<N-1; i++){
             int num = Integer.parseInt(br.readLine());
             max = Math.max(max, num);
 
-            if(st.peek()< num){
-                result+=num - st.pop();
-                st.push(num);
-            }else if(st.peek() > num){
-                st.pop();
-                st.push(num);
+            if(minimum< num){
+                result+=num - minimum;
+                minimum = num;
+            }else if(minimum > num){
+                minimum = num;
             }
         }
 
 
-        result += max-st.pop();
+        result += max-minimum;
 
 
         System.out.println(result);
