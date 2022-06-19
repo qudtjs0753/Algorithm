4c0bf7fb38f9b83496b21d3e5cbc51d582888346
qudtjs0753 <qudtjs0753@naver.com>
2022-06-19 10:37:30 +0900

    Baekjoon : 12852 bfs visit배열 사용 풀이

--- src/Baekjoon/Algo12852.java
+++ src/Baekjoon/Algo12852.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Queue;
 
 /**
@@ -13,61 +12,57 @@ import java.util.Queue;
 public class Algo12852 {
     static int N, count=0;
     static int[] order = new int[1_000_001];
-
+    static boolean[] visit = new boolean[1_000_001];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
         bfs();
 
-        ArrayList<Integer> result = getPath();
-        for(int i=result.size()-1; i>=0; i--){
-            sb.append(result.get(i)).append(" ");
-        }
+        String result = getPath();
         System.out.println(count);
-        System.out.println(sb);
+        System.out.println(result);
     }
 
     private static void bfs() {
         Queue<Integer> q = new ArrayDeque<>();
-        q.add(N);
+        q.add(1);
 
         while (!q.isEmpty()) {
             int current = q.poll();
-            if(current==1)return;
-            if(current<1)return;
-            if(current%3==0){
-                int next = current/3;
-                q.add(next);
-                if(order[next]==0){
-                    order[next] = current;
-                }
-            }
-            if(current%2==0){
-                int next = current/2;
-                q.add(next);
-                if(order[next]==0){
-                    order[next] = current;
-                }
-            }
+            if(current==N)return;
+            if(current*3<=N && !visit[current*3]){
+                int next = current*3;
+                addNext(q,current,next);
 
-            int next = current-1;
-            q.add(next);
-            if(order[next]==0){
-                order[next] = current;
+            }
+            if(current*2<=N && !visit[current*2]){
+                int next = current*2;
+                addNext(q,current,next);
+            }
+            if(current+1<=N && !visit[current+1]){
+                int next = current+1;
+                addNext(q,current,next);
             }
         }
     }
 
-    private static ArrayList<Integer> getPath() {
-        ArrayList<Integer> arr = new ArrayList<>();
-        int current = 1;
+    private static void addNext(Queue<Integer> q, int current, int next) {
+        visit[next] = true;
+        q.add(next);
+        if(order[next]==0){
+            order[next] = current;
+        }
+    }
+
+    private static String getPath() {
+        StringBuilder sb = new StringBuilder();
+        int current = N;
 
         while(true){
-           arr.add(current);
-            if(current==N){
-                return arr;
+            sb.append(current).append(" ");
+            if(current==1){
+                return sb.toString();
             }
             count++;
             current = order[current];
