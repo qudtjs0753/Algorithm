aac559b5d64107ec3d803793cac120fb62d65095
KBS <qudtjs0753@naver.com>
2023-03-25 13:12:16 +0900

    Refactor(swexpert): 광고시간정하기 문제 주석 추가
    최적화문제를 결정문제로 변환하는 문제다.
    여기서 사용한 아이디어는 다음과 같다.
    1. 각 피크타임의 시작점에서 출발하는 것이 그 피크타임에서의 중간에서 출발하는 것보다 겹치는 시간이 크거나 같다.
    2. hi를 upperbound에 걸어놓음으로서 lo가 선택한 피크타임의 시작점이 start의 시작점 + L보다 작거나 같은 최대값을 보도록 한다.

--- src/swexpert/binarysearch/AdvertisementTime.java
+++ src/swexpert/binarysearch/AdvertisementTime.java
@@ -21,13 +21,13 @@ public class AdvertisementTime {
         int T = Integer.parseInt(br.readLine());
 
         for (int testCase = 1; testCase <= T; testCase++) {
-            L = Integer.parseInt(br.readLine());
-            N = Integer.parseInt(br.readLine());
-            sum = new int[N + 1];
-            time = new int[N + 1][2];
+            L = Integer.parseInt(br.readLine()); //광고 진행 시간.
+            N = Integer.parseInt(br.readLine()); //피크타임 수.
+            sum = new int[N + 1]; //누적합을 저장한다.
+            time = new int[N + 1][2]; //각 피크타임별 시작시간, 종료시간을 저장한다.
             result = 0;
             for (int i = 1; i <= N; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
+                StringTokenizer st = new StringTokenizer(br.readLine()); 
                 int s = Integer.parseInt(st.nextToken());
                 int e = Integer.parseInt(st.nextToken());
                 sum[i] = sum[i - 1] + e - s;
@@ -50,20 +50,20 @@ public class AdvertisementTime {
         while (lo + 1 < hi) {
             int mid = (lo + hi) / 2;
 
-            if (!(L >= time[mid][0] - time[start][0])) {
+            if (!(L >= time[mid][0] - time[start][0])) { //hi를 upperbound로 설정해준다. -> 즉, lo가 lowerbound가 됨.
                 hi = mid;
             } else {
                 lo = mid;
             }
         }
 
-        int comp = sum[lo] - sum[start - 1];
+        int comp = sum[lo] - sum[start - 1]; //선택한 구간에서 겹치는 최대 시간.
 
         //끝점에서 시작점을 뺀 길이가 L보다 크거나 같을 때.
         if (L >= time[lo][1] - time[start][0]) {
             result = Math.max(result, comp);
             return;
         }
-        result = Math.max(result, comp - (time[lo][1] - (L + time[start][0])));
+        result = Math.max(result, comp - (time[lo][1] - (L + time[start][0]))); //L보다 클때는 그 차이만큼 빼주어야 한다.
     }
 }
