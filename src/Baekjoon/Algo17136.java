03d1eef0e8a5386c6262cbf0688913c5056b0884
qudtjs0753 <qudtjs0753@naver.com>
2023-04-10 16:00:27 +0900

    Add(baekjoon): 백트래킹 문제 풀이
    visit 배열을 사용했었는데, 겹치는 부분에 대해 invalid 체크를 하지 않아 여러번 틀렸었따.

---
+++ src/Baekjoon/Algo17136.java
@@ -0,0 +1,89 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Algo17136 {
+
+    static int result = Integer.MAX_VALUE;
+    static int[][] arr;
+    static boolean[][] visit;
+    static int[] count = new int[6];
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        arr = new int[10][10];
+        visit = new boolean[10][10];
+
+        Arrays.fill(count, 5);
+
+        for(int i=0; i<10; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<10; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        backtracking(0,0, 0);
+
+        if(result==Integer.MAX_VALUE) {
+            System.out.println(-1);
+        }else {
+            System.out.println(result);
+        }
+    }
+
+    private static void backtracking(int y, int x, int value) {
+        if(value>=result) return;
+
+        if(y>=9 && x>9) {
+            result = Math.min(value,result);
+            return;
+        }
+        //x값이 끝에 도달했으면 다음 y로 넘어간다.
+        if(x>9) {
+            backtracking(y+1,0, value);
+            return;
+        }
+        //y가 끝에 도달했다는 것은 모든 지점에 방문했다는 것.
+
+
+        if(arr[y][x]==0 || visit[y][x]) {
+            backtracking(y, x+1, value);
+            return;
+        }
+
+        for(int i=1; i<=5; i++) {
+            if(count[i]==0) continue;
+            if(isInvalid(y,x,i)) continue;
+            count[i] -= 1;
+            mark(y,x,i);
+            backtracking(y, x+1, value+1);
+            mark(y,x,i);
+            count[i] += 1;
+        }
+    }
+
+    //표기했다가 취소하는 과정.
+    private static void mark(int y, int x, int length) {
+        for(int i=y; i<y+length; i++) {
+            for(int j=x; j<x+length; j++) {
+                visit[i][j] = !visit[i][j];
+            }
+        }
+    }
+
+    private static boolean isInvalid(int y, int x, int length) {
+        if(y+length > 10 || x+length > 10) return true;
+        for(int i=y; i<y+length; i++) {
+            for(int j=x; j<x+length; j++) {
+                if(arr[i][j]==0 || visit[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
