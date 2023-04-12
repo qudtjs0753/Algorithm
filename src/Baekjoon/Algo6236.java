ad8367aa1725011da89e88d89e4f18bf4266ac3a
qudtjs0753 <qudtjs0753@naver.com>
2023-04-12 23:51:18 +0900

    Add(baekjoon): 이분탐색 풀이

---
+++ src/Baekjoon/Algo6236.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.*;
+public class Algo6236 {
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+    }
+}
