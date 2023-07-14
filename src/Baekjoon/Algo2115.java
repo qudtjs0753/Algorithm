3be7dae2d10f3459f1d790b1f54683fafe20624f
KBS <qudtjs0753@naver.com>
2023-07-14 23:28:23 +0900

    fix(baekjoon): 2115번 코드 개선

--- src/Baekjoon/Algo2115.java
+++ src/Baekjoon/Algo2115.java
@@ -2,11 +2,12 @@ package Baekjoon;
 
 import java.io.*;
 import java.util.*;
+
 public class Algo2115 {
 
-    static int N,M, result = 0;
+    static int N, M, result = 0;
     static boolean[][] map;
-    static int[][] wallsY,wallsX;
+    static int[][] wallsY, wallsX;
 
 
     public static void main(String[] args) throws IOException {
@@ -15,34 +16,30 @@ public class Algo2115 {
     }
 
     private static void solve() {
-        for(int i=1; i<N; i++) {
-            for(int j=1; j<M; j++) {
-                if(map[i-1][j]!=map[i][j]) {
-                    if(map[i][j]==map[i][j-1] && map[i-1][j]==map[i-1][j-1]) {
-                        wallsX[i][j] = wallsX[i][j-1]+1;
-                    }else {
-                        wallsX[i][j] = 1;
-                        result+=wallsX[i][j-1]/2;
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < M; j++) {
+                if (map[i - 1][j] != map[i][j]) {
+                    if (map[i][j] == map[i][j - 1] && map[i - 1][j] == map[i - 1][j - 1]) {
+                        wallsX[i][j] = wallsX[i][j - 1] + 1;
+                        continue;
                     }
-                }else {
-                    result += wallsX[i][j-1]/2;
+                    wallsX[i][j] = 1;
                 }
+                result += wallsX[i][j - 1] / 2;
             }
         }
 
 
-        for(int j=1; j<M; j++) {
-            for(int i=1; i<N; i++) {
-                if(map[i][j-1]!=map[i][j]) {
-                    if(map[i-1][j-1]==map[i][j-1] && map[i-1][j]==map[i][j]) {
-                        wallsY[i][j] = wallsY[i-1][j]+1;
-                    }else {
-                        wallsY[i][j] = 1;
-                        result+=wallsY[i-1][j]/2;
+        for (int j = 1; j < M; j++) {
+            for (int i = 1; i < N; i++) {
+                if (map[i][j - 1] != map[i][j]) {
+                    if (map[i - 1][j - 1] == map[i][j - 1] && map[i - 1][j] == map[i][j]) {
+                        wallsY[i][j] = wallsY[i - 1][j] + 1;
+                        continue;
                     }
-                }else {
-                    result += wallsY[i-1][j]/2;
+                    wallsY[i][j] = 1;
                 }
+                result += wallsY[i - 1][j] / 2;
             }
         }
 
@@ -59,10 +56,10 @@ public class Algo2115 {
         wallsY = new int[N][M];
         wallsX = new int[N][M];
 
-        for(int i=0; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             String input = br.readLine();
-            for(int j=0; j<M; j++) {
-                if(input.charAt(j)=='.') {
+            for (int j = 0; j < M; j++) {
+                if (input.charAt(j) == '.') {
                     map[i][j] = true;
                 }
             }
