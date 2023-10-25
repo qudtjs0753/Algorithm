84c21b57fc97f220b8dcdf94536297092250a584
KBS <qudtjs0753@naver.com>
2023-10-25 23:51:05 +0900

    feat(baekjoon): 12865번 배낭문제 top-down dp
    Signed-off-by: KBS <qudtjs0753@naver.com>

--- src/Baekjoon/Algo12865.java
+++ src/Baekjoon/Algo12865.java
@@ -10,41 +10,45 @@ public class Algo12865 {
     static int N, K;
     static Thing[] things;
     static int[][] dp;
+
     public static class Thing {
         int weight, value;
-        Thing(int weight, int value){
+
+        Thing(int weight, int value) {
             this.weight = weight;
             this.value = value;
         }
 
     }
-    public static int findKnapsack(){
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=K; j++){
-                if(things[i].weight > j){
-                    dp[i][j] = dp[i-1][j];
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j-things[i].weight]+things[i].value, dp[i-1][j]);
-                }
-            }
-        }
 
-        return dp[N][K];
+    public static int findKnapsack(int kindOfThing, int k) {
+        if (kindOfThing == N+1) return 0;
+
+        if (dp[kindOfThing][k]!=-1) return dp[kindOfThing][k];
+
+        if(k-things[kindOfThing].weight>=0) {
+            dp[kindOfThing][k] = Math.max(
+                    findKnapsack(kindOfThing+1,  k-things[kindOfThing].weight) + things[kindOfThing].value,
+                    dp[kindOfThing][k]);
+        }
+        return dp[kindOfThing][k] = Math.max(findKnapsack(kindOfThing+1, k), dp[kindOfThing][k]);
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        things = new Thing[N+1];
-        dp = new int[N+1][K+1];
+        things = new Thing[N + 1];
+        dp = new int[N + 1][K + 1];
 
-        for(int i=1; i<=N; i++){
+        for (int i = 1; i <= N; i++) {
             StringTokenizer input = new StringTokenizer(br.readLine(), " ");
             things[i] = new Thing(Integer.parseInt(input.nextToken()), Integer.parseInt(input.nextToken()));
+            Arrays.fill(dp[i],-1);
         }
 
-        System.out.println(findKnapsack());
+        System.out.println(findKnapsack(1,  K));
     }
 }
